ad9bac832b37f8808da94b31c24cf5d17a80158c
CheropS <sh.cherop@gmail.com>
2023-10-02 20:41:41 +0300

    add floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: annotated function that takes a float a
+and a float be as arguments returns their sum
+as as float'''
+
+
+def add(a: float, b: float) -> float:
+    '''Return sum of a and b'''
+    return a + b
