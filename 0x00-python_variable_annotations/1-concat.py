ec899ed5b7170b439194ce414ef2903a382222ce
CheropS <sh.cherop@gmail.com>
2023-10-02 20:46:03 +0300

    concatination strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: takes string1 and string2 and returns concated string
+arguments: str1: str
+           str2: str'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Returns concatenation of str1 and str2'''
+    return str1 + str2 
